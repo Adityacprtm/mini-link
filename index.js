463a06bb04b8413e20fb3ac54f9d3e8fb6847eca
Aditya Pratama <pratamaditya7@gmail.com>
2021-02-20 12:12:47 +0700

    🚀 update to mini-link

--- index.js
+++ index.js
@@ -22,86 +22,86 @@ app.use(express.static("./public"));
 const notFoundPath = path.join(__dirname, "public/404.html");
 
 app.get("/:id", async (req, res, next) => {
-   const { id: slug } = req.params;
-   try {
-      const url = await urls.findOne({ slug });
-      if (url) {
-         return res.redirect(url.url);
-      }
-      return res.status(404).sendFile(notFoundPath);
-   } catch (error) {
-      return res.status(404).sendFile(notFoundPath);
-   }
+  const { id: slug } = req.params;
+  try {
+    const url = await urls.findOne({ slug });
+    if (url) {
+      return res.redirect(url.url);
+    }
+    return res.status(404).sendFile(notFoundPath);
+  } catch (error) {
+    return res.status(404).sendFile(notFoundPath);
+  }
 });
 
 const schema = yup.object().shape({
-   slug: yup
-      .string()
-      .trim()
-      .matches(/^[\w\-]+$/i)
-      .min(5)
-      .max(20),
-   url: yup.string().trim().url().required(),
+  slug: yup
+    .string()
+    .trim()
+    .matches(/^[\w\-]+$/i)
+    .min(5)
+    .max(20),
+  url: yup.string().trim().url().required(),
 });
 
 app.post(
-   "/url",
-   slowDown({
-      windowMs: 30 * 1000, // 30 seconds
-      delayAfter: 2, // allow 2 requests per 30 sec, then...
-      delayMs: 500, // begin adding 500ms of delay per request above 2
-   }),
-   rateLimit({
-      windowMs: 30 * 1000, // 30 seconds
-      max: 2, // limit each IP to 2 requests per windowMs
-   }),
-   async (req, res, next) => {
-      let { slug, url } = req.body;
-      try {
-         await schema.validate({
-            slug,
-            url,
-         });
-         if (!slug) {
-            slug = nanoid(5);
-         } else {
-            const existing = await urls.findOne({ slug });
-            if (existing) {
-               throw new Error("Slug is in use. 😔");
-            }
-         }
-         const newUrl = {
-            url,
-            slug,
-         };
-         const created = await urls.insert(newUrl);
-         res.json(created);
-      } catch (error) {
-         next(error);
+  "/url",
+  slowDown({
+    windowMs: 30 * 1000, // 30 seconds
+    delayAfter: 2, // allow 2 requests per 30 sec, then...
+    delayMs: 500, // begin adding 500ms of delay per request above 2
+  }),
+  rateLimit({
+    windowMs: 30 * 1000, // 30 seconds
+    max: 2, // limit each IP to 2 requests per windowMs
+  }),
+  async (req, res, next) => {
+    let { slug, url } = req.body;
+    try {
+      await schema.validate({
+        slug,
+        url,
+      });
+      if (!slug) {
+        slug = nanoid(5);
+      } else {
+        const existing = await urls.findOne({ slug });
+        if (existing) {
+          throw new Error("Slug is in use. 😔");
+        }
       }
-   }
+      const newUrl = {
+        url,
+        slug,
+      };
+      const created = await urls.insert(newUrl);
+      res.json(created);
+    } catch (error) {
+      next(error);
+    }
+  }
 );
 
 // Not Found Handler
 app.use((req, res, next) => {
-   res.status(404).sendFile(notFoundPath);
+  res.status(404).sendFile(notFoundPath);
 });
 
 // Error Handler
 app.use((error, req, res, next) => {
-   if (error.status) {
-      res.status(error.status);
-   } else {
-      res.status(500);
-   }
-   res.json({
-      success: false,
-      message: error.message,
-      stack: process.env.NODE_ENV === "production" ? "🥞" : error.stack,
-   });
+  if (error.status) {
+    res.status(error.status);
+  } else {
+    res.status(500);
+  }
+  res.json({
+    success: false,
+    message: error.message,
+    stack: process.env.NODE_ENV === "production" ? "🥞" : error.stack,
+  });
 });
 
 // Server Listening
 app.listen(port, () => {
-   console.log(`Listening at http://localhost:${port}`);
+  console.log(`Listening at ${port}`);
 });
